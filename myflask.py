556e5c67e7ab849b32f3e366617d918baeb52da5
eamin123 <eamin.ea@gmail.com>
2023-01-19 22:36:08 -0500

    Update myflask.py

--- myflask.py
+++ myflask.py
@@ -1,9 +1,13 @@
 from werkzeug.wrappers import Request, Response
 from flask import Flask, render_template, request
+import csv 
+import pandas as pd
+
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def form():
     return render_template('form.html')
  
@@ -18,11 +22,31 @@ def data():
         name = request.form["Name"]
         city = request.form["City"]
         country = request.form["Country"]
-        
+        #country = openmyfile()
        # print(request.form["email"])
        # form_data = request.form
-        return render_template('data.html',name=name,city=city,country=country)
+        df = pd.read_csv('ACX-RLI-Query.csv')
+        df.to_csv('ACX-RLI-Query.csv', index=None)
+        data = pd.read_csv('ACX-RLI-Query.csv')
+        data2 = data[data['Synopsis'].str.contains(name,case=False)]
  
+        return render_template('data.html',name=name,city=city,country=country, tables=[data2.to_html()],titles=[''])
+
+def openmyfile():
+    # opening the CSV file
+        with open('ACX-RLI-Query.csv', mode ='r')as file:
+            # reading the CSV file
+            csvFile = csv.reader(file)
+            return csvFile
+            # displaying the contents of the CSV file
+            #for lines in csvFile:
+            #    print(lines)
+
+
+
+    
+    
+    
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('localhost', 9000, app)
